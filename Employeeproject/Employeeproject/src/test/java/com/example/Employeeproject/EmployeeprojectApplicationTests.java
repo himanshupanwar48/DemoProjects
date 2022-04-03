77f5661838cf267825b0285d464c1ee4363a5959
himanshupanwar48 <85739841+himanshupanwar48@users.noreply.github.com>
2022-04-03 23:31:28 +0530

    1 file uploaded

---
+++ Employeeproject/Employeeproject/src/test/java/com/example/Employeeproject/EmployeeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Employeeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
